e3555dd9ef2dd1c746f4db64b21bfd059406913d
Alexandre Doucet <alexandredoucet.fr@gmail.com>
2022-11-30 16:45:46 +0100

    menu-burger

---
+++ script.js
@@ -0,0 +1,9 @@
+const btnBurger = document.querySelector(".icone_burger");
+const menuBurger = document.querySelector(".menu_burger");
+
+btnBurger.addEventListener("click", function () {
+
+menuBurger.classList.toggle("visible");
+
+
+})
