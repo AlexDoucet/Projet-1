50f3362fe0416681fd7205faf549f11377e66bfc
Alexandre Doucet <alexandredoucet.fr@gmail.com>
2022-12-01 11:30:44 +0100

    sous-menu

--- script.js
+++ script.js
@@ -6,4 +6,25 @@ btnBurger.addEventListener("click", function () {
 menuBurger.classList.toggle("visible");
 
 
-})
+});
+
+const btnCompet = document.querySelector(".compet");
+const menuCompet = document.querySelector(".menu_compet");
+
+btnCompet.addEventListener("click", function () {
+
+menuCompet.classList.toggle("visible1");
+
+});
+
+
+
+const btnCompet1 = document.querySelector(".compet1");
+const menuCompet1 = document.querySelector(".menu_compet1");
+
+btnCompet1.addEventListener("click", function () {
+
+menuCompet1.classList.toggle("visible1");
+
+});
+
