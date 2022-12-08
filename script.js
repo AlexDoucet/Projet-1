b7853aed20126d89416e64d79518d780d308e69d
Alexandre Doucet <alexandredoucet.fr@gmail.com>
2022-12-08 17:19:19 +0100

    Dark mode

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-
-
 const btnBurger = document.querySelector(".icone_burger");
 const menuBurger = document.querySelector(".menu_burger");
 
 btnBurger.addEventListener("click", function () {
 
-menuBurger.classList.toggle("visible");
-
-
-});
-
-
+    menuBurger.classList.toggle("visible");
+    
+    
+    });
 
 const btnCompet = document.querySelector(".compet");
 const menuCompet = document.querySelector(".menu_compet");
@@ -22,7 +18,6 @@ menuCompet.classList.toggle("visible1");
 });
 
 
-
 const btnCompet1 = document.querySelector(".compet1");
 const menuCompet1 = document.querySelector(".menu_compet1");
 
@@ -31,17 +26,6 @@ btnCompet1.addEventListener("click", function () {
 menuCompet1.classList.toggle("visible1");
 
 });
-
-// const btnUser = document.querySelector(".fa-solid fa-user");
-// const menuLogin = document.querySelector(".popup");
-
-// btnUser.addEventListener("click", function () {
-
-// menuLogin.classList.toggle("visible2");
-
-// });
-
-
 
 document.querySelector(".user").addEventListener("click", function(){
     document.querySelector(".popup").classList.add("active2");
@@ -55,6 +39,70 @@ document.querySelector(".user1").addEventListener("click", function(){
     document.querySelector(".popup").classList.add("active2");
 });
 
-document.querySelector(".popup .close-btn").addEventListener("click", function(){
-    document.querySelector(".popup").classList.remove("active2");
+const btnDark = document.querySelectorAll(".dark_mode");
+
+for(let i =0; i < btnDark.length; i++){
+
+btnDark[i].addEventListener("click", ()=>{
+    
+    const body = document.body;
+
+    if(body.classList.contains("light")){
+
+        body.classList.add("dark");
+        body.classList.remove("light");
+        btnDark[i].innerHTML = "Light mode"
+
+    }else if(body.classList.contains("dark")){
+
+        body.classList.add("light");
+        body.classList.remove("dark");
+        btnDark[i].innerHTML = "Dark mode"
+    }
+
+})};
+
+
+
+document.forms["login"].addEventListener("submit", function(){
+
+    let inputs = this;
+    const user = document.querySelector(".user");
+
+if(inputs["email"].value === "admin@google.com" && inputs["password"].value === "admin1991"){
+    
+    alert(`Bienvenue Alexandre !`);
+function removeIcon(){
+
+            const header = document.querySelector("header");
+            const user = document.querySelector(".user");
+            user.parentNode.removeChild(user)};
+
+            document.createElement("div");
+            header.appendChild("div");
+}else{
+    alert("Email et/ou mot de passe invalide")
+}
+
 });
+
+// Dark Mode //
+
+
+
+
+
+
+
+
+
+
+const focus = document.querySelectorAll(".article1");
+
+for(let i =0; i < focus.length; i++){
+
+    focus[i].addEventListener("click", function(){
+        focus[i].classList.toggle("zoom");
+
+})};
+
